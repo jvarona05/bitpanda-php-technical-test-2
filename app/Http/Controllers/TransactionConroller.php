7c3ff5b044de2a351334627341e1e147b59fc8de
José <jose.varona05@gmail.com>
2020-04-16 18:56:49 +0200

    add api documnetation

--- app/Http/Controllers/TransactionConroller.php
+++ app/Http/Controllers/TransactionConroller.php
@@ -7,8 +7,22 @@ use App\Http\Requests\TransactionRequest;
 use App\Services\TransactionFactory;
 use App\Http\Resources\TransactionResource;
 
+/**
+ * @group Transactions
+ */
 class TransactionConroller extends Controller
 {
+    /**
+     * Get Transactions 
+     * 
+     * Returns users filtered by country and status.
+     *  
+     * @queryParam source required The source of the data(db or csv). Example: db
+     * 
+     * @param TransactionRequest $request
+     *  
+     * @return \Illuminate\Http\Response
+     */
     public function __invoke(TransactionRequest $request, TransactionFactory $factory)
     {
         $transactions = $factory->make($request->source)->getAll();
