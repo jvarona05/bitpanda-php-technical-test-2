b44604124eec0a621866e6b83a11dd81dad069c0
José <jose.varona05@gmail.com>
2020-04-16 15:07:16 +0200

    Update TransactionConroller.php

--- app/Http/Controllers/TransactionConroller.php
+++ app/Http/Controllers/TransactionConroller.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Transaction;
+use Illuminate\Support\Facades\Storage;
 
 class TransactionConroller extends Controller
 {
     public function __invoke()
     {
+        return $file = Storage::get('transactions.csv');
         return Transaction::all();
     }
 }
